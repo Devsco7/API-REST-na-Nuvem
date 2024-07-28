5c82034e13bf299011b757ea0e48b0a9fe65d825
Devsco7 <dev.sco@outlook.com>
2024-07-28 18:16:26 -0300

    Primeira tentativa de publicação no Railway

---
+++ src/main/java/me/dio/controller/exception/GlobalExceptionHanlder.java
@@ -0,0 +1,35 @@
+package me.dio.controller.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHanlder {
+
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHanlder.class);
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handle(IllegalArgumentException businessException) {
+        return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> handleNotFoundException(NoSuchElementException notFoundException) {
+        return new ResponseEntity<>("Resource ID not found.", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleUnexpectedException(Throwable unexpectedException) {
+        var message = "Unexpected server error, see the logs.";
+        logger.error(message,unexpectedException);
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
